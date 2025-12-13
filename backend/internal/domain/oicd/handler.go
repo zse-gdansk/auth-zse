315b5cf3c7a2751c52e45b303141a33b2d1face2
Jakub <45879657+zeedivx@users.noreply.github.com>
2025-12-13 16:16:57 +0100

    feat: detailed OpenID configuration

--- backend/internal/domain/oicd/handler.go
+++ backend/internal/domain/oicd/handler.go
@@ -4,8 +4,28 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func OpenIDConfigurationHandler() fiber.Handler {
+func OpenIDConfigurationHandler(domain string) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{})
+		return c.JSON(fiber.Map{
+			"issuer": domain,
+
+			"authorization_endpoint": domain + "/oauth/authorize",
+			"token_endpoint":         domain + "/oauth/token",
+			"userinfo_endpoint":      domain + "/oauth/userinfo",
+			"jwks_uri":               domain + "/.well-known/jwks.json",
+
+			"scopes_supported": []string{
+				"openid", "profile", "email", "tp", "ke",
+			},
+
+			"response_types_supported": []string{"code"},
+			"grant_types_supported": []string{
+				"authorization_code",
+				"refresh_token",
+			},
+
+			"subject_types_supported":               []string{"public"},
+			"id_token_signing_alg_values_supported": []string{"RS256"},
+		})
 	}
 }
